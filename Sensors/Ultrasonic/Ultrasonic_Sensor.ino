fad461f76bc29e83886229fd4c76fa7fafa5f6e8
Raghav Kumar <raghavcast@gmail.com>
2018-08-23 22:27:15 +0530

    Ultrasonic
    Added Ultrasonic Sensor code

---
+++ Sensors/Ultrasonic/Ultrasonic_Sensor.ino
@@ -0,0 +1,27 @@
+#define trigpin 13
+#define echopin 12
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(trigpin,OUTPUT);
+  pinMode(echopin,INPUT);
+}
+
+void loop() {
+  long duration,distance;
+  digitalWrite(trigpin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigpin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin,LOW);
+  duration = pulseIn(echopin,HIGH);
+  distance = (duration/2)/29.1;
+
+  if (distance >= 200 || distance <= 0) {
+    Serial.println("Out of Range");
+  } else {
+    Serial.print(distance);
+    Serial.println("cm");
+  }
+  delay(500);
+}
